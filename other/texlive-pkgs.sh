f7dc4f9128646ba3431764dbd523eff04084d194
htlsne <htlsne@users.noreply.github.com>
2017-05-03 15:42:14 +0900

    Add texlive install script

---
+++ other/texlive-pkgs.sh
@@ -0,0 +1,9 @@
+tlmgr install \
+  latexmk thmtools cleveref environ \
+  tex-gyre silence tcolorbox enumitem \
+  newtx newpx \
+  adobemapping \
+  ipaex-type1 \
+  arev bera mathdesign \
+  ctablestack filehook type1cm trimspaces luatex85 mweights fontaxes \
+  pgf ms xcolor etoolbox xetex boondox biblatex
