545eec8fbc7106ac047b14a09e30145904a84345
htlsne <htlsne23@gmail.com>
2018-04-07 22:36:00 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,9 @@ DOTFILES_DIR=$(cd $(dirname $0);pwd)
 
 cd $HOME
 
-files=(.zsh .zshrc .zshrc.fzf .zshrc.`uname` .gitconfig .latexmkrc .tmux.conf .bundle)
+files_to_link=(.zsh .zshrc .zshrc.fzf .zshrc.`uname` .gitconfig .latexmkrc .tmux.conf .bundle)
+files_other=(.gitconfig.os .vimrc)
+files=(${files_to_link[@]} ${files_other[@]})
 
 exist_symlinks=()
 exist_files=()
@@ -14,7 +16,6 @@ exist_dirs=()
 for file in ${files[@]}; do
   if [ -e $HOME/$file ]; then
     if [ -L $HOME/$file ]; then
-      echo "symlink $file"
       exist_symlinks+=($file)
     elif [ -f $HOME/$file ]; then
       exist_files+=($file)
@@ -35,7 +36,7 @@ while true; do
     [Yy]* )
       for file in ${exist_all[@]}; do
         echo "Removing $file"
-        rm -f $HOME/$file
+        rm -rf $HOME/$file
       done
       break ;;
     [Nn]* )
@@ -46,47 +47,24 @@ while true; do
   esac
 done
 
-for file in ${files[@]}; do
+# make symlink
+for file in ${files_to_link[@]}; do
   echo Linking $file
   ln -s $DOTFILES_DIR/$file $HOME/$file
 done
 
+# git
 echo Linking .gitignore
 mkdir -p $HOME/$XDG_CONFIG_HOME/git
 ln -s $DOTFILES_DIR/gitignore-global $HOME/$XDG_CONFIG_HOME/git/ignore
-
 echo Linking .gitconfig.os
-ln -s $DOTFILES_DIR/.gitconfig.`uname` .gitconfig.os
+ln -s $DOTFILES_DIR/.gitconfig.`uname` $HOME/.gitconfig.os
 
 # vim
-
-vimfiles=(.vim .vimrc)
-
-for file in ${vimfiles[@]}; do
-  if [ -e $HOME/$file ]; then
-    if [ -L $HOME/$file ]; then
-      type=symlink
-    elif [ -f $HOME/$file ]; then
-      type=file
-    else
-      type=directory
-    fi
-    echo Already $type $HOME/$file exists.
-    read -p 'Remove this? [y/n]' Answer
-    case $Answer in
-      [Yy]* )
-        rm -rf $HOME/$file ;;
-      [Nn]* )
-        ;;
-      * )
-        echo Please answer YES or NO.  ;;
-    esac
-  fi
-done
-
 git clone https://github.com/htlsne/vimrc.git $HOME/.vim
 
 # neovim
+echo Linking $XDG_CONFIG_HOME/nvim
 ln -s $HOME/.vim $XDG_CONFIG_HOME/nvim
 
 # tmux
