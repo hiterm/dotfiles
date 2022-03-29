0793f44e78c771ddcaa3e48cde21573b4b266a02
hiterm <hiterm23@gmail.com>
2022-03-29 23:31:23 +0900

    Change key bindings for wezterm

--- other/wezterm.lua
+++ other/wezterm.lua
@@ -1,3 +1,5 @@
+local wezterm = require("wezterm")
+
 return {
 	color_scheme = "nord",
 	unix_domains = {
@@ -5,5 +7,50 @@ return {
 			name = "unix",
 		},
 	},
+	default_gui_startup_args = { "connect", "unix" },
 	font_size = 12.0,
+
+	disable_default_key_bindings = true,
+	leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+		{ key = "t", mods = "CTRL|SHIFT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "Tab", mods = "CTRL", action = wezterm.action({ ActivateTabRelative = 1 }) },
+		{ key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTabRelative = -1 }) },
+		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
+		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+
+		{ key = "k", mods = "ALT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "j", mods = "ALT", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
+		{ key = "h", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
+		{ key = "l", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
+		{ key = "h", mods = "ALT|CTRL", action = wezterm.action({ MoveTabRelative = -1 }) },
+		{ key = "l", mods = "ALT|CTRL", action = wezterm.action({ ActivateTabRelative = 1 }) },
+		{ key = "k", mods = "ALT|CTRL", action = "ActivateCopyMode" },
+		{ key = "j", mods = "ALT|CTRL", action = wezterm.action({ PasteFrom = "PrimarySelection" }) },
+		{ key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
+		{ key = "-", mods = "ALT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+		{ key = "=", mods = "ALT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+		{ key = "h", mods = "ALT|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "l", mods = "ALT|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+		{ key = "k", mods = "ALT|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "j", mods = "ALT|SHIFT", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "h", mods = "ALT|SHIFT|CTRL", action = wezterm.action({ AdjustPaneSize = { "Left", 1 } }) },
+		{ key = "l", mods = "ALT|SHIFT|CTRL", action = wezterm.action({ AdjustPaneSize = { "Right", 1 } }) },
+		{ key = "k", mods = "ALT|SHIFT|CTRL", action = wezterm.action({ AdjustPaneSize = { "Up", 1 } }) },
+		{ key = "j", mods = "ALT|SHIFT|CTRL", action = wezterm.action({ AdjustPaneSize = { "Down", 1 } }) },
+
+		{ key = "s", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x13" }) },
+		{ key = " ", mods = "LEADER", action = "QuickSelect" },
+		{ key = "]", mods = "LEADER", action = "ActivateCopyMode" }, -- "[" in JIS
+		{ key = "2", mods = "LEADER|SHIFT", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) }, -- '"' in JIS
+		{ key = "5", mods = "LEADER|SHIFT", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) }, -- '"' in JIS
+	},
 }
